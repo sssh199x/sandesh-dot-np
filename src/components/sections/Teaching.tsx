c9bfa5cc356a79b6c851666269e5231027ce6586
sssh199x <sandeshhamal5890@gmail.com>
2026-04-19 19:48:24 +0545

    Restore right position, shrink watermark to 60%

--- src/components/sections/Teaching.tsx
+++ src/components/sections/Teaching.tsx
@@ -62,7 +62,7 @@ const collegeBg = "/images/teaching/informatics-college-pokhara.webp";
 function CollegeBgMark() {
   return (
     <div
-      className="pointer-events-none absolute inset-[1rem] bg-contain bg-right bg-no-repeat opacity-[0.08]"
+      className="pointer-events-none absolute inset-0 bg-[length:60%] bg-[position:right_-1rem_center] bg-no-repeat opacity-[0.08]"
       style={{ backgroundImage: `url(${collegeBg})` }}
       aria-hidden="true"
     />
